71941a0f0eee1292bea7d6c367283a26fc23cb35
dastudent <l3farrow@gmail.com>
2020-04-02 12:07:54 +0100

    dice pic roll

---
+++ js/dicegame1.js
@@ -0,0 +1,18 @@
+// function myFunction(){
+//     var randomNumber    =   Math.floor(Math.random() * 6)+1//1-6 roll
+//     var diceImages="images/dice"+randomNumber+".png" //pull dice image 1-6 based on randomNumber var
+ 
+//     document.getElementById('diceImage').src=diceImages;
+// }
+// </script>
+ 
+// <img id="diceImage" src="C:\\Users\\user\\Desktop\\html_js\\images\\dice3.png"/>
+// <button onclick="myFunction()">test</button>
+
+// var randomNumber=(Math.floor(Math.random()*6))+1//1-6 roll
+// var diceImages="/Users/dastudent/Desktop/MasterCourse/week3/day3/images/dice"+randomNumber+".png" //pull dice image 1-6 based on randomNumber var
+
+// // document.querySelector('.img').setAttribute('src', diceImages)
+
+// // alert("hello")
+
